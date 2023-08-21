4cc1d8db5a2e14ed7b52656c55dd696b849d4e67
YusifNajat <82729993+YusifNajat@users.noreply.github.com>
2023-08-21 03:28:57 +0300

    ok

--- src/compositionApi/WatcherApi.vue
+++ src/compositionApi/WatcherApi.vue
@@ -3,27 +3,47 @@
 <input type="text" v-model="name" placeholder="name">
 <input type="text" v-model="fname" placeholder="fname">
 <input type="text" v-model="lname" placeholder="lname">
+
+<input type="text" v-model="firstname" placeholder="first name">
+<input type="text" v-model="lastname" placeholder="last name">
+
+<input type="text" v-model="options.op" placeholder="options">
     </div>
 </template>
 <!-- wathers bakar de bo 
 away hamu katek state bgordre lagare gorani har shtek la statetaka
 -->
 <script>
-import {ref,watch} from 'vue'
+import _ from 'lodash'
+import {ref,watch,reactive,toRefs} from 'vue'
     export default {
         name:"WatcherApi",
         setup(){
+            
+const state=reactive({
+    firstname:"",
+    lastname:'',
+    options:{
+        op:''
+    }
+})
+watch(()=>_.cloneDeep(state.options),(nvalue,ovalue)=>{
+console.log('nvalue f', nvalue)
+console.log('ovalue f', ovalue)
+},{deep:true})
+
             const fname=ref('')
             const lname=ref('')
-            watch([fname,lname],(newValues,oldValues)=>{
-                console.log('newValuefname', newValues[0])
-                console.log('oldValuefname', oldValues[0])
-                console.log('newValuelname', newValues[1])
-                console.log('oldValuelname', oldValues[1])
-            },{immediate:true})
+            // watch([fname,lname],(newValues,oldValues)=>{
+            //     console.log('newValuefname', newValues[0])
+            //     console.log('oldValuefname', oldValues[0])
+            //     console.log('newValuelname', newValues[1])
+            //     console.log('oldValuelname', oldValues[1])
+            // },{immediate:true})
             return{
                 fname,
-                lname
+                lname,
+                ...toRefs(state)
             }
         },
         data(){
